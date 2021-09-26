451ffa013efb9ee3bdc1bf5ff3f2ca6c88a53b27
Nitin Shrivastav <38551867+nitinshrivastav@users.noreply.github.com>
2021-09-26 12:32:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def jkfkjf():
+    return render_template('program.html')
+@app.route("/info",methods=['GET','POST'])
+def fjkfkjf():
+    if(request.method=='POST'):
+        num1=int(request.form['a'])
+        num2=(request.form['b'])
+        total=num1+num2
+        return render_template('program.html',answer=total)
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
